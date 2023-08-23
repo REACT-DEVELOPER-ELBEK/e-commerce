b05ab9e1f23e53362f365e5bd6a1a8354d0c0b3f
Yusupov Elbek <elbeky829@gmail.com>
2023-08-23 16:31:17 +0500

    home page added

---
+++ js/home.js
@@ -0,0 +1,18 @@
+let burger = document.querySelector('#burger')
+let menu = document.querySelector('.burger__menu__wrapper')
+let hero = document.querySelector('.hero')
+let discounts = document.querySelector('.discounts')
+
+burger.addEventListener('click', () => {
+    menu.style.display = 'block'
+    hero.style.display = 'none'
+    discounts.style.marginTop = '220px'
+
+    if(menu = menu.style.display === 'block'){
+        burger.addEventListener('click', () =>{
+            menu.style.display = 'none'
+            hero.style.display = 'block'
+        })
+        
+    }
+})
