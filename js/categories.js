7e272de7eba6771699649692d2da297479d2d765
Yusupov Elbek <elbeky829@gmail.com>
2023-08-26 01:16:46 +0500

    discount__card (class) mapped

--- js/categories.js
+++ js/categories.js
@@ -1,15 +1,79 @@
 let burger = document.querySelector('#burger')
 let menu = document.querySelector('.burger__menu__wrapper')
-let hero = document.querySelector('.hero')
-let discounts = document.querySelector('.discounts')
 
 burger.addEventListener('click', () => {
     menu.style.display = 'block'
-    hero.style.display = 'none'
-    discounts.style.marginTop = '220px'
 })
+
+let cards = [
+    {
+        id: 'protains',
+        image: '../img/catalog1.png'
+    },
+
+    {
+        id: 'protains',
+        image: '../img/catalog2.png'
+    },
+
+    {
+        id: 'protains',
+        image: '../img/catalog3.png'
+    },
+
+    {
+        id: 'protains',
+        image: '../img/catalog4.png'
+    },
+
+    {
+        id: 'protains',
+        image: '../img/catalog5.png'
+    },
+
+    {
+        id: 'protains',
+        image: '../img/catalog6.png'
+    },
+
+    {
+        id: 'protains',
+        image: '../img/catalog7.png'
+    },
+
+    {
+        id: 'protains',
+        image: '../img/catalog8.png'
+    },
+
+    {
+        id: 'protains',
+        image: '../img/catalog9.png'
+    },
+
+    {
+        id: 'protains',
+        image: '../img/catalog10.png'
+    },
+
+    {
+        id: 'protains',
+        image: '../img/catalog11.png'
+    },
+
+    {
+        id: 'protains',
+        image: '../img/catalog12.png'
+    }
+]
 
 let catalogCards = document.querySelector('.catalog__cards')
 
-let arr = [...catalogCards.children]
-console.log(arr[3])
+let str = ''
+cards.map((catalog) => {
+    str+=`
+    <a href="#"><img src=${catalog.image} alt="img"></a>
+    `
+})
+
+catalogCards.innerHTML = str
--- js/home.js
+++ js/home.js
@@ -1,7 +1,6 @@
 let burger = document.querySelector('#burger')
 let menu = document.querySelector('.burger__menu__wrapper')
 let hero = document.querySelector('.hero')
-let discounts = document.querySelector('.discounts')
 
 burger.addEventListener('click', () => {
     menu.style.display = 'block'
@@ -9,23 +8,95 @@ burger.addEventListener('click', () => {
     discounts.style.marginTop = '220px'
 })
 
-let specialDeals = document.querySelector('.special__discount__cards')
-let childsToObj = specialDeals.children
-let arr = [...childsToObj]
-console.log(arr);
+let getDiscountCard = document.querySelector('.discount__cards')
 
-    // for(let i = 0; i<specialDeals.children.length; i++){
-    //     let arrays = Object.assign({}, [specialDeals.children[i]])
-    // console.log(arrays)
-    // }
+let discountCards = [
+    {
+        img: '../img/discount1.png',
+        priceWithCard: '44,50 ₽', 
+        priceWithCash: '50,50 ₽', 
+        name: 'Г/Ц Блинчики с мясом вес, Россия',
+        raiting: [
+            '../img/full-star.svg',
+            '../img/full-star.svg',
+            '../img/empty-star.svg',
+            '../img/empty-star.svg',
+            '../img/empty-star.svg'
+        ]
+    },
 
-let recentBought = document.querySelector(".recent__cards")
-let recentBoughtChilds = recentBought.children
-let recentBoughtArray = [...recentBoughtChilds]
-let getLastChilds = recentBoughtArray.splice(-4)
-console.log(getLastChilds)
+    {
+        img: '../img/discount2.png',
+        priceWithCard: '44,50 ₽', 
+        priceWithCash: '50,50 ₽', 
+        name: 'Молоко ПРОСТОКВАШИНО паст. питьевое цельное отборное...',
+        raiting: [
+            '../img/full-star.svg',
+            '../img/full-star.svg',
+            '../img/full-star.svg',
+            '../img/empty-star.svg',
+            '../img/empty-star.svg'
+        ],
+    },
 
-let discount = document.querySelector(".discounts")
+    {
+        img: '../img/discount3.png',
+        priceWithCard: '44,50 ₽', 
+        priceWithCash: '50,50 ₽', 
+        name: 'Колбаса сырокопченая МЯСНАЯ ИСТОРИЯ Сальчичон и Тоскан...',
+        raiting: [
+            '../img/full-star.svg',
+            '../img/full-star.svg',
+            '../img/full-star.svg',
+            '../img/full-star.svg',
+            '../img/full-star.svg'
+        ],
+    },
+
+    {
+        img: '../img/discount4.png',
+        priceWithCard: '44,50 ₽', 
+        priceWithCash: '50,50 ₽', 
+        name: 'Сосиски вареные МЯСНАЯ ИСТОРИЯ Молочные и С сыро...',
+        raiting: [
+            '../img/full-star.svg',
+            '../img/full-star.svg',
+            '../img/full-star.svg',
+            '../img/full-star.svg',
+            '../img/full-star.svg'
+        ],
+    }
+]
+
+let disCard = ''
+discountCards.map((card) => {
+    disCard+=`
+    <div class="discount__card">
+    <div class="like"><img src="./img/like.svg" alt=""></div>
+    <div class="discounts__discount"><p>-50%</p></div>
+    <img src=${card.img} alt="image">
+    <div class="discount__card__text">
+        <div class="discount__price">
+            <p>${card.priceWithCard}</p>
+            <h3>${card.priceWithCash}</h3>
+
+            <h4>С картой</h4>
+            <h4>Обычная</h4>
+        </div>
+
+        <p>${card.name}</p>
+
+        <div class="discount__raiting">
+            
+        </div>
+
+        <button>В корзину</button>
+    </div>
+</div>
+    `
+})
+
+getDiscountCard.innerHTML = disCard
 
 
 
